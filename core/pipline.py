71916b2c3673e6c6e73071e2113d507bef7e5611
alaa.zreekie <alaa.zreekie@hiast.edu.sy>
2026-02-17 11:48:44 +0300

    Refactor: Add pipeline so every thing is added and modify inside the pipe so the main does not cjange

---
+++ core/pipline.py
@@ -0,0 +1,28 @@
+from utils.video_utils import read_video, save_video
+from core.track.tracker import Tracker
+from core.detection.detector import Detector
+from utils.config_loader import cfg
+
+class Pipeline:
+    def __init__(self, config):
+        self.config = config
+        self.tracker = Tracker()
+        self.detector = Detector(config['settings']['model_path'])
+
+    
+    def run(self):
+        #1. Read video
+        frames = read_video(self.config['settings']['input_video_path'])
+        #2. Detection
+        detections = self.detector.detect_frames(frames)
+        #3. Track objects
+        tracks = self.tracker.get_object_tracks(detections)
+
+        #4. Draw annotation
+        output_frames = self.tracker.draw_tracks(frames, tracks)
+
+        #5. Save video
+        save_video(output_frames, self.config['settings']['output_path'])
+
+
+
--- main.py
+++ main.py
@@ -1,18 +1,6 @@
 from utils.config_loader import cfg
-from core.detection.detector import Detector
-from utils.video_utils import save_video, read_video
-from ultralytics import YOLO
-import os
-
-def main():
-    video_path = cfg['settings']['input_video_path']
-    save_path = os.path.join(cfg['settings']['output_path'], 'output_video.mp4')
-    model_path = cfg['settings']['model_path']
-    conf_thres = cfg['settings']['confidence_threshold']
-    iou_thres = cfg['settings']['iou_threshold']
-
-    model = YOLO(model_path)
-    results = model.track(video_path, save=True, project=save_path, name="inference", exist_ok=True, conf=conf_thres, iou=iou_thres,output=save_path)
+from core.pipeline import Pipeline
 
 if __name__ == "__main__":
-    main()
+    pipeline = Pipeline(cfg['settings'])
+    pipeline.run()
